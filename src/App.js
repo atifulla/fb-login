6ea1cfa8d2f65085c3554796b9059c7a2b4cb9cc
Atif-coder <atifulla1@gmail.com>
2021-05-05 15:56:48 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,52 @@ function App() {
           </div>
         </div>
 
-        <div className="w-1/2">Righttttt</div>
+        <div className="w-1/2 flex items-center">
+          <div class="flex flex-col flex items-center ">
+            <div>
+              <div class="flex flex-col w-full bg-white rounded shadow-lg h-full p-6 m-40 bg-gray-400 ">
+                <div>
+                  <div class="field-group mb-4 md:w-full">
+                    <input
+                      class="field border-blue-50"
+                      type="email"
+                      name="email"
+                      id="email"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div class="field-group mb-6 md:w-full">
+                    <input
+                      class="field"
+                      type="password"
+                      name="password"
+                      id="password"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <button class="btn btn-teal mx-auto" type="submit">
+                    Login
+                  </button>
+                </div>
+                <div>
+                  <a class="link link-grey w-full text-center" href="/login">
+                    Forgotten password?
+                  </a>
+                </div>
+                <div>
+                  <div>
+                    <button class="btn btn-teal mx-auto" type="submit">
+                      Login
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <p>Create a Page for a celebrity, band or business.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
