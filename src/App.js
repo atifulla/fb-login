d9cb347d4bbb8df0d6da2e86769b517fd2ada5b6
Atif-coder <atifulla1@gmail.com>
2021-05-05 13:29:28 +0530

    changes made by atif

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div class="flex min-h-screen">
-  <div>
-  <div class="flex flex-col justify-center items-center min-h-screen w-3/4">
-  <h1 class="text-6xl text-blue-500 font-bold">facebook</h1>
-  <h3 class="text-3xl font-normal  items-center p-4 text-justify">Facebook helps you connect and share with the people in your life.</h3>
-</div>
-
-  </div>
-  <div>2</div>
-</div>
+      <div class="flex min-h-screen bg-yellow-200">
+        <div>
+          <div class="flex flex-col justify-center items-center min-h-screen w-3/4 bg-blue-300">
+            <div >
+            <h1 class="text-6xl text-blue-500 font-bold bg-gray-400">facebook</h1>
+            <h3 class="text-3xl font-normal  items-center w-10/12 text-justify bg-gray-400">
+              Facebook helps you connect and share with the people in your life.
+            </h3>
+          </div>
+          </div>
+        </div>
+        <div>2</div>
+      </div>
     </div>
   );
 }
